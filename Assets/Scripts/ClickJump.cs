7b7c77e65456b7a40cf84c2803e04e069cd30928
Maxim Polyaev <polyaev.maks@ya.ru>
2020-01-19 17:49:34 +0300

    first commit

---
+++ Assets/Scripts/ClickJump.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickJump : MonoBehaviour
+{
+    public Vector2 force;
+    public Component mainCharacter;
+
+    private Rigidbody2D mainCharacterRB2D;
+    private void OnMouseUp()
+    {
+        mainCharacterRB2D.AddForce(force, ForceMode2D.Force);
+    }
+
+    private void Start()
+    {
+        mainCharacterRB2D = mainCharacter.GetComponent<Rigidbody2D>();
+    }
+}
---
+++ Assets/Scripts/ViewTitleGame.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewTitleGame : MonoBehaviour
+{
+    public float speed;
+    public float checkPos;
+
+    private bool viewTitle = true;
+
+    private RectTransform rectTransform;
+
+    private void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        if (rectTransform.offsetMin.y > checkPos && viewTitle)
+        {
+            rectTransform.offsetMin += new Vector2(rectTransform.offsetMin.x, speed);
+            rectTransform.offsetMax += new Vector2(rectTransform.offsetMax.x, speed);
+        }
+        else
+        {
+            viewTitle = false;
+        }
+    }
+}
